11d0f7e737d91b9e881e4725e405ee5f35e035e5
Vi <1149443+mistodon@users.noreply.github.com>
2017-02-22 02:02:18 +0000

    Finally add type parameter to StoreKey
    It ain't pretty though.

--- mod.rs
+++ mod.rs
@@ -2,9 +2,15 @@ use std::env::current_exe;
 use std::io::Result;
 use std::path::PathBuf;
 
-mod store;
+use glium::{Program, Texture2d};
 
+
+mod store;
 pub use self::store::*;
+
+pub use rendering::mesh::{Mesh, SpriteVertex};
+pub type Shader = Program;
+pub type Texture = Texture2d;
 
 
 pub fn path_to_asset(asset_name: &str) -> Result<PathBuf>
--- store.rs
+++ store.rs
@@ -1,6 +1,7 @@
 use std::collections::hash_map::{HashMap, DefaultHasher};
 use std::hash::{Hash, Hasher};
-
+use std::marker::PhantomData;
+use std::sync::Arc;
 
 pub struct Store<T>
 {
@@ -21,21 +22,34 @@ impl<T> Store<T>
         hasher.finish()
     }
 
-    pub fn insert(&mut self, name: &'static str, item: T) -> StoreKey
+    pub fn insert(&mut self, name: &'static str, item: T) -> StoreKey<T>
     {
         let index = Self::hash_key(name);
         self.map.insert(index, item);
-        StoreKey { index: index }
+        StoreKey { index: index, _phantom: PhantomData }
     }
 
-    pub fn get(&self, key: StoreKey) -> Option<&T>
+    pub fn get(&self, key: StoreKey<T>) -> Option<&T>
     {
         self.map.get(&key.index)
     }
 }
 
-#[derive(Clone, Copy, Debug)]
-pub struct StoreKey
+#[derive(Debug)]
+pub struct StoreKey<T>
 {
-    index: u64
+    index: u64,
+    _phantom: PhantomData<Arc<T>>
 }
+
+impl<T> Clone for StoreKey<T>
+{
+    fn clone(&self) -> Self
+    {
+        StoreKey { index: self.index, _phantom: PhantomData }
+    }
+}
+
+impl<T> Copy for StoreKey<T> {}
+unsafe impl<T> Send for StoreKey<T> {}
+unsafe impl<T> Sync for StoreKey<T> {}
