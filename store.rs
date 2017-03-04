599bc9e8f18975bcabbb8a654bf7739f8e15c37a
Vi <1149443+mistodon@users.noreply.github.com>
2017-03-04 15:24:07 +0000

    Add ResourceStore trait and use linear storage for Store

--- store.rs
+++ store.rs
@@ -1,45 +1,12 @@
-use std::collections::hash_map::{HashMap, DefaultHasher};
-use std::hash::{Hash, Hasher};
 use std::marker::PhantomData;
-use std::sync::Arc;
+use std::ops::Index;
 
-pub struct Store<T>
-{
-    map: HashMap<u64, T>
-}
-
-impl<T> Store<T>
-{
-    pub fn new() -> Self
-    {
-        Store { map: HashMap::new() }
-    }
-
-    fn hash_key(name: &'static str) -> u64
-    {
-        let mut hasher = DefaultHasher::new();
-        name.hash(&mut hasher);
-        hasher.finish()
-    }
-
-    pub fn insert(&mut self, name: &'static str, item: T) -> StoreKey<T>
-    {
-        let index = Self::hash_key(name);
-        self.map.insert(index, item);
-        StoreKey { index: index, _phantom: PhantomData }
-    }
-
-    pub fn get(&self, key: StoreKey<T>) -> Option<&T>
-    {
-        self.map.get(&key.index)
-    }
-}
 
 #[derive(Debug)]
 pub struct StoreKey<T>
 {
-    index: u64,
-    _phantom: PhantomData<Arc<T>>
+    index: usize,
+    _phantom: PhantomData<T>
 }
 
 impl<T> Clone for StoreKey<T>
@@ -53,3 +20,79 @@ impl<T> Clone for StoreKey<T>
 impl<T> Copy for StoreKey<T> {}
 unsafe impl<T> Send for StoreKey<T> {}
 unsafe impl<T> Sync for StoreKey<T> {}
+
+
+pub trait ResourceStore<T>
+{
+    fn add(&mut self, item: T) -> StoreKey<T>;
+    fn get(&self, key: StoreKey<T>) -> &T;
+}
+
+pub struct Store<T>
+{
+    items: Vec<T>
+}
+
+impl<T> Store<T>
+{
+    pub fn new() -> Self
+    {
+        Store
+        {
+            items: Vec::new()
+        }
+    }
+}
+
+
+impl<T> ResourceStore<T> for Store<T>
+{
+    fn add(&mut self, item: T) -> StoreKey<T>
+    {
+        let index = self.items.len();
+        self.items.push(item);
+        StoreKey { index: index, _phantom: PhantomData }
+    }
+
+    fn get(&self, key: StoreKey<T>) -> &T
+    {
+        self.items.index(key.index)
+    }
+}
+
+
+#[cfg(test)]
+mod tests
+{
+    use super::*;
+
+
+    #[test]
+    fn can_insert_items_into_store()
+    {
+        let mut store = Store::new();
+        let _: StoreKey<usize> = store.add(5);
+    }
+
+    #[test]
+    fn can_fetch_items_from_store()
+    {
+        let mut store = Store::new();
+        let key = store.add(10);
+        assert_eq!(*store.get(key), 10);
+    }
+
+    fn sendy_sync<T>(_: T)
+        where T: Send + Sync
+    {
+
+    }
+
+    #[test]
+    fn store_keys_can_be_send_between_threads()
+    {
+        let mut store = Store::new();
+        let key = store.add(15);
+        sendy_sync(key);
+    }
+}
